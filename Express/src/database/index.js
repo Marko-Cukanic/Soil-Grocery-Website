97b8a908ab1dcc92d7588717bf7afd6ca85b15e2
Marko-Cukanic <s4008040@student.rmit.edu.au>
2024-05-23 16:21:46 +1000

    merge

--- Express/src/database/index.js
+++ Express/src/database/index.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql' // Specify your dialect here
+    dialect: 'mysql'
   }
 );
 
@@ -18,22 +18,12 @@ db.sequelize = sequelize;
 
 // Import models
 db.User = require('./models/user')(sequelize, DataTypes);
-// Import other models similarly
+db.Product = require('./models/product')(sequelize, DataTypes);
+db.CartItem = require('./models/cart_item')(sequelize, DataTypes);
+db.IsLoggedIn = require('./models/isLoggedIn')(sequelize, DataTypes);
+db.Review = require('./models/review')(sequelize, DataTypes);
+db.WeeklySpecial = require('./models/weeklyspecials')(sequelize, DataTypes);
 
-// Set up associations
-db.User.hasMany(db.Review, { foreignKey: 'user_id' });
-db.Review.belongsTo(db.User, { foreignKey: 'user_id' });
 
-db.Product.hasMany(db.Review, { foreignKey: 'product_id' });
-db.Review.belongsTo(db.Product, { foreignKey: 'product_id' });
-
-db.User.hasOne(db.IsLoggedIn, { foreignKey: 'user_id' });
-db.IsLoggedIn.belongsTo(db.User, { foreignKey: 'user_id' });
-
-db.Product.belongsToMany(db.User, { through: db.CartItem, foreignKey: 'product_id' });
-db.User.belongsToMany(db.Product, { through: db.CartItem, foreignKey: 'user_id' });
-
-db.Product.hasMany(db.WeeklySpecial, { foreignKey: 'product_id' });
-db.WeeklySpecial.belongsTo(db.Product, { foreignKey: 'product_id' });
 
 module.exports = db;
--- Express/src/database/models/isLoggedIn.js
+++ Express/src/database/models/isLoggedIn.js
@@ -1,15 +1,21 @@
-module.exports = (sequelize, DataTypes) =>
-    sequelize.define("is_logged_in", {
-      user_id: {
-        type: DataTypes.INTEGER(11),
-        primaryKey: true
-      },
-      is_logged_in: {
-        type: DataTypes.TINYINT(1),
-        allowNull: false,
-        defaultValue: 0
+module.exports = (sequelize, DataTypes) => {
+  const IsLoggedIn = sequelize.define("IsLoggedIn", {
+    userId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      references: {
+        model: 'Users', // 'Users' refers to table name
+        key: 'id'
       }
-    }, {
-      timestamps: false
-    });
-  
+    },
+    loggedIn: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  }, {
+    timestamps: false
+  });
+
+  return IsLoggedIn;
+};
